8e5107b892423c2db8b598ece2a41c90b8633180
choco30 <47217764+choco30@users.noreply.github.com>
2023-05-25 16:09:13 +0530

    Update Audit_class.py

--- Airflow/Audit_class.py
+++ Airflow/Audit_class.py
@@ -14,37 +14,37 @@ class audit_class_bq_ds_auditcntrl:
 
 	def audit_start(self,dag_name,task,table,status,load_date,record_count):
 		bq_client=bigquery.Client()
-		run_id_query = "select cast(coalesce(max(run_id),cast(0 as string)) as INT64) + 1 from `{0}.{1}.{2}`".format("corp-services-krnospbi-svc-dev","bq_ds_auditcntrl","KRONOSPBI_DAG_EXEC_STATUS")
+		run_id_query = "select cast(coalesce(max(run_id),cast(0 as string)) as INT64) + 1 from `{0}.{1}.{2}`".format("project_name","bq_ds_auditcntrl","KRONOSPBI_DAG_EXEC_STATUS")
 		run_id_query_sel =bq_client.query(run_id_query)
 		job_id=""
 		for row in run_id_query_sel:
 			job_id=str(row[0])
 			break  
-		insert_query="insert into `{0}.{1}.{2}` Values('{3}','{4}','{5}','{6}',{7},'{8}',current_datetime('UTC+5:30'),NULL,'{9}')".format('corp-services-krnospbi-svc-dev','bq_ds_auditcntrl','KRONOSPBI_DAG_EXEC_STATUS',job_id,dag_name,task,table,record_count,status,load_date)
+		insert_query="insert into `{0}.{1}.{2}` Values('{3}','{4}','{5}','{6}',{7},'{8}',current_datetime('UTC+5:30'),NULL,'{9}')".format('project_name','bq_ds_auditcntrl','KRONOSPBI_DAG_EXEC_STATUS',job_id,dag_name,task,table,record_count,status,load_date)
 		result_query=bq_client.query(insert_query)
 		result_query.result()
 
 	def audit_end_fail(self,dag_name,task,table,status,load_date):
 		bq_client = bigquery.Client()
-		run_id_query = "select cast(coalesce(max(run_id),cast(0 as string)) as INT64) from `{0}.{1}.{2}` where DAG_NAME='{3}' and TASK='{4}'and LOAD_DATE='{5}' and table='{6}' ".format("corp-services-krnospbi-svc-dev","bq_ds_auditcntrl","KRONOSPBI_DAG_EXEC_STATUS",dag_name,task,load_date,table)
+		run_id_query = "select cast(coalesce(max(run_id),cast(0 as string)) as INT64) from `{0}.{1}.{2}` where DAG_NAME='{3}' and TASK='{4}'and LOAD_DATE='{5}' and table='{6}' ".format("project_name","bq_ds_auditcntrl","Table",dag_name,task,load_date,table)
 		run_id_query_sel =bq_client.query(run_id_query)
 		job_id=""
 		for row in run_id_query_sel:
 			job_id=str(row[0])
 			break	
-		update_query="update  `{0}.{1}.{2}` set END_DATE_TIME=current_datetime('UTC+5:30'),STATUS='{3}' where RUN_ID='{4}' and DAG_NAME='{5}' and TASK='{6}'and LOAD_DATE='{7}' and table='{8}'".format("corp-services-krnospbi-svc-dev","bq_ds_auditcntrl","KRONOSPBI_DAG_EXEC_STATUS",status,job_id,dag_name,task,load_date,table)
+		update_query="update  `{0}.{1}.{2}` set END_DATE_TIME=current_datetime('UTC+5:30'),STATUS='{3}' where RUN_ID='{4}' and DAG_NAME='{5}' and TASK='{6}'and LOAD_DATE='{7}' and table='{8}'".format("project_name","bq_ds_auditcntrl","Table",status,job_id,dag_name,task,load_date,table)
 		result_query = bq_client.query(update_query)
 		result_query.result()
 	
 	def audit_end_success(self,dag_name,task,table,status,load_date,record_count):
 		bq_client = bigquery.Client()
-		run_id_query = "select cast(coalesce(max(run_id),cast(0 as string)) as INT64) from `{0}.{1}.{2}` where DAG_NAME='{3}' and TASK='{4}'and LOAD_DATE='{5}' and table='{6}' ".format("corp-services-krnospbi-svc-dev","bq_ds_auditcntrl","KRONOSPBI_DAG_EXEC_STATUS",dag_name,task,load_date,table)
+		run_id_query = "select cast(coalesce(max(run_id),cast(0 as string)) as INT64) from `{0}.{1}.{2}` where DAG_NAME='{3}' and TASK='{4}'and LOAD_DATE='{5}' and table='{6}' ".format("project_name","bq_ds_auditcntrl","Table",dag_name,task,load_date,table)
 		run_id_query_sel =bq_client.query(run_id_query)
 		job_id=""
 		for row in run_id_query_sel:
 			job_id=str(row[0])
 			break
-		update_query="update  `{0}.{1}.{2}` set END_DATE_TIME=current_datetime('UTC+5:30'),STATUS='{3}',RECORD_COUNT={9} where RUN_ID='{4}' and DAG_NAME='{5}' and TASK='{6}'and LOAD_DATE='{7}' and table='{8}'".format("corp-services-krnospbi-svc-dev","bq_ds_auditcntrl","KRONOSPBI_DAG_EXEC_STATUS",status,job_id,dag_name,task,load_date,table,record_count)
+		update_query="update  `{0}.{1}.{2}` set END_DATE_TIME=current_datetime('UTC+5:30'),STATUS='{3}',RECORD_COUNT={9} where RUN_ID='{4}' and DAG_NAME='{5}' and TASK='{6}'and LOAD_DATE='{7}' and table='{8}'".format("Project_name","bq_ds_auditcntrl","Table",status,job_id,dag_name,task,load_date,table,record_count)
 		result_query =bq_client.query(update_query)
 		result_query.result()
 
